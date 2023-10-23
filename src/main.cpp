0c2d8c7ccc0fda476d73925783412c6ccb55771e
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-10-23 06:29:11 -0300

    Work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,15 @@
 
 #include <string>
 
+static void my_panic(void *udata, const char *msg)
+{
+  (void)udata;
+
+  fprintf(stderr, "*** FATAL ERROR: %s\n", (msg ? msg : "no message"));
+  fflush(stderr);
+  abort();
+}
+
 size_t writefunc(void *ptr, size_t size, size_t nmemb, std::string *buffer)
 {
   buffer->append(static_cast<char *>(ptr), size * nmemb);
@@ -48,29 +57,81 @@ error:
   return ret;
 }
 
-/*
-var req = new XMLHttpRequest();
-req.responseType = 'json';
-req.open('GET', url, true);
-req.onload  = function() {
-   var jsonResponse = req.response;
-   // do something with jsonResponse
+class JSONRPCWrapper
+{
+public:
+  char *call()
+  {
+    return strdup("ok");
+  }
 };
-req.send(null);
-*/
+
+JSONRPCWrapper jsonrpc;
+
+duk_ret_t call(duk_context *ctx)
+{
+  duk_push_this(ctx);
+  duk_get_prop_string(ctx, -1, "\xff"
+                               "\xff"
+                               "JSONRPC");
+
+  JSONRPCWrapper *x = static_cast<JSONRPCWrapper *>(duk_to_pointer(ctx, -1));
+  if (x)
+  {
+    char *result = x->call();
+    duk_push_string(ctx, result);
+    return 1;
+  }
+
+  duk_pop(ctx);
+
+  return 0;
+}
+
+duk_ret_t jsonrpc_destructor(duk_context *ctx)
+{
+  return 0;
+}
+
+duk_ret_t jsonrpc_constructor(duk_context *ctx)
+{
+  if (!duk_is_constructor_call(ctx))
+  {
+    return DUK_RET_TYPE_ERROR;
+  }
+
+  duk_push_this(ctx);
+  duk_push_pointer(ctx, &jsonrpc);
+  duk_put_prop_string(ctx, -2, "\xff"
+                               "\xff"
+                               "JSONRPC");
+
+  duk_push_c_function(ctx, jsonrpc_destructor, 1);
+  duk_set_finalizer(ctx, -2);
+
+  return 0;
+}
 
 int main()
 {
   curl_global_init(CURL_GLOBAL_DEFAULT);
 
-  duk_context *ctx = duk_create_heap_default();
+  duk_context *ctx = duk_create_heap(NULL, NULL, NULL, NULL, my_panic);
+
+  duk_push_c_function(ctx, jsonrpc_constructor, 0);
+  duk_push_bare_object(ctx);
+  duk_push_c_function(ctx, call, DUK_VARARGS);
+  duk_put_prop_string(ctx, -2, "call");
+  duk_put_prop_string(ctx, -2, "prototype");
+  duk_put_global_string(ctx, "JSONRPC");
 
   duk_push_c_function(ctx, native_print, 1 /*nargs*/);
   duk_put_global_string(ctx, "print");
 
   duk_push_c_function(ctx, native_fetch, DUK_VARARGS);
   duk_put_global_string(ctx, "fetch");
-  duk_eval_string_noresult(ctx, "print(fetch('https://httpbin.org/get'));");
+
+  duk_eval_string_noresult(ctx, "var rpc = new JSONRPC; print('JSONRPC result: ' + rpc.call())");
 
   duk_destroy_heap(ctx);
 
